3ca0f19ef749932e5deaf2d60e0a5894cf61039b
Muhammad Kaleem Ullah <kaleemullah601@gmail.com>
2022-07-21 05:52:42 +0200

    Add new files to make it run like a package

---
+++ setup.py
@@ -0,0 +1,6 @@
+#Please execute this file first befor running the project
+import os, sys
+fpath = os.path.dirname(os.path.abspath(__file__))
+print(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import vhbdetector
